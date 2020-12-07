f737a91416725e2fd17fe21541f9139dceb2e1e9
Fredrik Johansen <fredrik1206@gmail.com>
2020-12-07 10:07:11 +0100

    Added feature to open dir

--- FolderViewer/Form1.Designer.cs
+++ FolderViewer/Form1.Designer.cs
@@ -24,13 +24,34 @@
 		/// </summary>
 		private void InitializeComponent() {
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.treeViewResult = new System.Windows.Forms.TreeView();
+			this.buttonOpenDir = new System.Windows.Forms.Button();
 			this.SuspendLayout();
+			// 
+			// treeViewResult
+			// 
+			this.treeViewResult.Location = new System.Drawing.Point(12, 12);
+			this.treeViewResult.Name = "treeViewResult";
+			this.treeViewResult.Size = new System.Drawing.Size(395, 524);
+			this.treeViewResult.TabIndex = 0;
+			// 
+			// buttonOpenDir
+			// 
+			this.buttonOpenDir.Location = new System.Drawing.Point(431, 486);
+			this.buttonOpenDir.Name = "buttonOpenDir";
+			this.buttonOpenDir.Size = new System.Drawing.Size(100, 50);
+			this.buttonOpenDir.TabIndex = 1;
+			this.buttonOpenDir.Text = "Open Dir";
+			this.buttonOpenDir.UseVisualStyleBackColor = true;
+			this.buttonOpenDir.Click += new System.EventHandler(this.buttonOpenDir_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(816, 515);
+			this.ClientSize = new System.Drawing.Size(820, 548);
+			this.Controls.Add(this.buttonOpenDir);
+			this.Controls.Add(this.treeViewResult);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
 			this.Text = "FolderViewer";
@@ -39,6 +60,9 @@
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TreeView treeViewResult;
+		private System.Windows.Forms.Button buttonOpenDir;
 	}
 }
 
--- FolderViewer/Form1.cs
+++ FolderViewer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,33 @@ namespace FolderViewer {
 			InitializeComponent();
 		}
 
+		private void buttonOpenDir_Click(object sender, EventArgs e) {
+			FolderBrowserDialog fileDialog = new FolderBrowserDialog();
+			DialogResult resultDialog = fileDialog.ShowDialog();
+
+			if (resultDialog != DialogResult.Cancel) {
+				// Creating the main node
+				TreeNode mainNode = new TreeNode();
+				mainNode.Name = "main";
+				mainNode.Text = "Main Node";
+
+				this.treeViewResult.Nodes.Add(mainNode);
+
+				string[] dirNames = Directory.GetDirectories(fileDialog.RootFolder);
+
+				// Creating sub nodes
+				foreach (string item in dirNames) {
+					TreeNode subNode = new TreeNode();
+					mainNode.Name = item.ToLower();
+					mainNode.Text = item;
+
+					mainNode.Nodes.Add(subNode);
+
+				}
+
+				
+
+			}
+		}
 	}
 }
