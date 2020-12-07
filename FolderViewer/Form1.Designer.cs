e2bb911d68ceb4d762fe9a14cfee3ba91cb109c2
Fredrik Johansen <fredrik1206@gmail.com>
2020-12-07 09:43:36 +0100

    Updated with icon

--- FolderViewer/Form1.Designer.cs
+++ FolderViewer/Form1.Designer.cs
@@ -23,10 +23,19 @@
 		///  the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.SuspendLayout();
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(816, 515);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "FolderViewer";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
