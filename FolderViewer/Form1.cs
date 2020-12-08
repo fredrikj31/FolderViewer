9fdebb11cca967eba5e43bb33acc605c4d786f30
Fredrik Johansen <fredrik1206@gmail.com>
2020-12-08 08:10:43 +0100

    Fixed path :)

--- FolderViewer/Form1.cs
+++ FolderViewer/Form1.cs
@@ -15,6 +15,25 @@ namespace FolderViewer {
 			InitializeComponent();
 		}
 
+		private void getDirs(string path, TreeNode parent) {
+			string[] dirNames = Directory.GetDirectories(path: path);
+
+			// Creating sub nodes
+			foreach (string item in dirNames) {
+				TreeNode subNode = new TreeNode();
+				subNode.Name = item.ToLower();
+				// Splitting the name
+				string[] splitName = item.Split('\\');
+
+				subNode.Text = splitName[splitName.Length - 1];
+
+				parent.Nodes.Add(subNode);
+
+				getDirs(item, subNode);
+
+			}
+		}
+
 		private void buttonOpenDir_Click(object sender, EventArgs e) {
 			FolderBrowserDialog fileDialog = new FolderBrowserDialog();
 			DialogResult resultDialog = fileDialog.ShowDialog();
@@ -23,28 +42,11 @@ namespace FolderViewer {
 				// Creating the main node
 				TreeNode mainNode = new TreeNode();
 				mainNode.Name = "main";
-				mainNode.Text = "Selected Folder";
+				mainNode.Text = fileDialog.SelectedPath;
 
 				this.treeViewResult.Nodes.Add(mainNode);
 
-				string[] dirNames = Directory.GetDirectories(fileDialog.SelectedPath);
-
-				// Creating sub nodes
-				foreach (string item in dirNames) {
-					TreeNode subNode = new TreeNode();
-					subNode.Name = item.ToLower();
-					// Splitting the name
-					string[] splitName = item.Split('\\');
-
-
-					subNode.Text = ;
-
-					mainNode.Nodes.Add(subNode);
-
-				}
-
-				
-
+				getDirs(fileDialog.SelectedPath, mainNode);
 			}
 		}
 	}
